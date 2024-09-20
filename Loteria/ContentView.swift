2c0bae5be19caf0b487303c159b92c8392094714
Tavo Avirez <gustavo.avila@gmail.com>
2024-09-19 18:54:09 -0600

    Initial Commit

---
+++ Loteria/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Loteria
+//
+//  Created by Gustavo Juarez on 19/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Loteria/LoteriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoteriaApp.swift
+//  Loteria
+//
+//  Created by Gustavo Juarez on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LoteriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
