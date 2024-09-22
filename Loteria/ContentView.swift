8d8539036237d27dafe8c1eb7cc757f97925b827
Tavo Avirez <gustavo.avila@gmail.com>
2024-09-22 01:48:29 -0600

    separacion de modelos

--- Loteria/ContentView.swift
+++ Loteria/ContentView.swift
@@ -4,20 +4,194 @@
 //
 //  Created by Gustavo Juarez on 19/09/24.
 //
-
 import SwiftUI
+import AVFoundation
+import Combine
 
 struct ContentView: View {
+    @StateObject private var gameModel = GameModel() // Asegúrate de que GameModel esté bien definido
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Sección de cartas usadas
+            UsedCards(gameModel: gameModel)
+            Spacer()
+            
+            // Binding para actualizar las cartas usadas
+            CardAppear(gameModel: gameModel)
+            
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(0)
+        .background(.backgroundApp)
     }
 }
+
+struct CardAppear: View {
+    @ObservedObject var gameModel: GameModel
+    
+    var body: some View {
+        HStack {
+            // Sección izquierda (Botón)
+            VStack {
+                Button(action: {
+                    if gameModel.gameStarted {
+                        gameModel.pauseGame()
+                    }
+                    gameModel.showOptions = true
+                }) {
+                    Image(systemName: "hourglass")
+                        .frame(maxWidth: .infinity, maxHeight: 80)
+                        .font(.largeTitle)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .sheet(isPresented: $gameModel.showOptions) {                    
+                    OptionsView(gameModel: gameModel)
+                }
+
+                if gameModel.gameStarted || gameModel.gamePaused {
+                    Button(action: {
+                        gameModel.resetGame()
+                    }) {
+                        Image(systemName: "repeat")
+                            .frame(maxWidth: .infinity, maxHeight: 80)
+                            .font(.largeTitle)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+            }
+            .frame(width: 80, height:.infinity )
+
+            // Sección central (Contenido principal)
+            VStack {
+                if gameModel.showImage {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                                 .trim(from: 0.0, to: gameModel.progress)
+                                 .stroke(Color.blue, lineWidth: 6.0)
+                                 .animation(.linear, value: gameModel.progress)
+                                 .frame(maxWidth: .infinity, maxHeight: 349)
+                   
+                        Image(gameModel.cardName)
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.horizontal, 8)
+                            .transition(.slide)
+                            .onTapGesture {
+                                if gameModel.gameStarted {
+                                    gameModel.pauseGame()
+                                } else {
+                                    gameModel.startTimer()
+                                }
+                            }
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+                Spacer()
+                if !gameModel.gameStarted && !gameModel.showImage {
+                    Button(action: {
+                        gameModel.startTimer()
+                    }) {
+                        Text("Iniciar")
+                            .padding()
+                            .background(.backgroundStartButton)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                } else {
+                    Button(action: {
+                        gameModel.resetGame()
+                    }) {
+                        Text("Siguiente")
+                            .padding()
+                            .background(.backgroundResetButton)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(0)
+
+             VStack {
+                Button(action: {
+                    // Acción para el botón izquierdo
+                }) {
+                    Rectangle()
+                    
+                    
+                }
+            }
+            .padding(0)
+            .frame(width: 70, height: .infinity )
+        }
+        .onAppear {
+            gameModel.resetGame()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct UsedCards: View {
+    @ObservedObject var gameModel: GameModel // Usar el modelo existente
+    
+    var body: some View {
+        ScrollViewReader { scrollProxy in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(gameModel.cartasUsadas) { card in
+                        Image(card.nombre) // Asegúrate de que las imágenes estén en tu proyecto
+                            .resizable()
+                            .frame(width: 100, height: 100) // Ajusta el tamaño según necesites
+                            .padding(1) // Agrega un poco de espacio entre las imágenes
+                            .shadow(radius: 5) // Añade una sombra para dar un efecto de profundidad
+                            .id(card.id) // Asegúrate de que cada imagen tenga un ID único
+                    }
+                }
+                .padding() // Agrega espacio alrededor del HStack
+            }
+            .frame(height: 120) // Ajusta la altura del ScrollView según sea necesario
+            .onChange(of: gameModel.cartasUsadas) { _ in
+                if let lastCard = gameModel.cartasUsadas.last {
+                    withAnimation {
+                        scrollProxy.scrollTo(lastCard.id, anchor: .trailing) // Desplaza hacia la última carta añadida
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+// modal de opciones del timer
+struct OptionsView: View {
+    @ObservedObject var gameModel: GameModel // Observa el modelo de juego
+    @Environment(\.dismiss) var dismiss // Permite cerrar la vista modal
+    
+    var body: some View {
+        NavigationView {
+            List(1...15, id: \.self) { option in
+                Button(action: {
+                    gameModel.changeInterval = TimeInterval(option) // Asegúrate de que sea TimeInterval
+                    dismiss() // Cierra el modal al seleccionar una opción
+                }) {
+                    Text(String(option))
+                }
+            }
+            .navigationTitle("Selecciona una Opción")
+            .navigationBarItems(trailing: Button("Cerrar") {
+                dismiss() // Cierra el modal al presionar "Cerrar"
+            })
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
---
+++ Loteria/Services/GameModel.swift
@@ -0,0 +1,176 @@
+//
+//  GameModel.swift
+//  Loteria
+//
+//  Created by Gustavo Juarez on 22/09/24.
+//
+
+import Combine
+import SwiftUI
+import AVFoundation
+
+class GameModel: ObservableObject {
+    @Published var cartasUsadas: [Carta] = []
+    @Published var gameStarted = false
+    @Published var gamePaused = false
+    @Published var changeInterval: TimeInterval = 2
+    @Published var loteriaCartasEnJuego: [Carta] = []
+    @Published var showImage = false
+    @Published var cardName: String = ""
+    // Propiedad para manejar el sonido
+    @Published var soundModel: SoundModel?
+    @Published var showOptions = false
+    @Published var progress: CGFloat = 0.0
+    
+    var timer: AnyCancellable?
+    var timerRectangle: Timer?
+    var timeRemaining: TimeInterval = 0
+    
+
+
+    struct Carta: Identifiable, Equatable{
+        let id = UUID() // Identificador único
+        let nombre: String
+    }
+
+
+    
+    
+    // Lista de cartas completas
+    @Published var loteriaCartasCompletas = [
+        Carta(nombre: "alacran"), Carta(nombre: "apache"), Carta(nombre: "arana"),
+        Carta(nombre: "arbol"), Carta(nombre: "arpa"), Carta(nombre: "bandera"),
+        Carta(nombre: "bandolon"), Carta(nombre: "barril"), Carta(nombre: "bota"),
+        Carta(nombre: "borracho"), Carta(nombre: "botella"), Carta(nombre: "calavera"),
+        Carta(nombre: "camaron"), Carta(nombre: "cantarito"), Carta(nombre: "campana"),
+        Carta(nombre: "catrin"), Carta(nombre: "cazo"), Carta(nombre: "chalupa"),
+        Carta(nombre: "corazon"), Carta(nombre: "corona"), Carta(nombre: "cotorro"),
+        Carta(nombre: "dama"), Carta(nombre: "diablito"), Carta(nombre: "escalera"),
+        Carta(nombre: "estrella"), Carta(nombre: "gallo"), Carta(nombre: "garza"),
+        Carta(nombre: "gorrito"), Carta(nombre: "jaras"), Carta(nombre: "luna"),
+        Carta(nombre: "maceta"), Carta(nombre: "mano"), Carta(nombre: "melon"),
+        Carta(nombre: "muerte"), Carta(nombre: "mundo"), Carta(nombre: "musico"),
+        Carta(nombre: "negrito"), Carta(nombre: "nopal"), Carta(nombre: "pajaro"),
+        Carta(nombre: "palma"), Carta(nombre: "paraguas"), Carta(nombre: "pera"),
+        Carta(nombre: "pescado"), Carta(nombre: "pino"), Carta(nombre: "rana"),
+        Carta(nombre: "rosa"), Carta(nombre: "sandia"), Carta(nombre: "sirena"),
+        Carta(nombre: "sol"), Carta(nombre: "soldado"), Carta(nombre: "tambor"),
+        Carta(nombre: "valiente"), Carta(nombre: "venado"), Carta(nombre: "violoncello")
+    ]
+    
+
+    // Este método se llama para reiniciar las cartas en juego
+     func resetGame() {
+        stopTimer()
+        stopTimerRectangle()
+        showImage = false
+        gameStarted = false
+        gamePaused = false
+        timeRemaining = changeInterval
+        progress = 0.0 // Reinicia el progreso
+        
+        cartasUsadas.removeAll()
+        loteriaCartasEnJuego = loteriaCartasCompletas.map { Carta(nombre: $0.nombre) }
+        
+    }
+
+    func stopTimer() {
+        gameStarted = false
+        timer?.cancel() // Detén el temporizador
+        timer = nil // Limpia el temporizador
+    }
+
+    func stopTimerRectangle() {
+        timerRectangle?.invalidate()
+        timerRectangle = nil
+    }
+
+    func startTimerRectangle() {
+        progress = 0.0
+        timerRectangle = Timer.scheduledTimer(withTimeInterval: changeInterval / 100.0, repeats: true) { _ in
+            if self.progress < 1.0 {                
+                self.progress += 0.01 // Incrementa el progreso
+            } else {
+                self.stopTimerRectangle() // Detiene el temporizador al completar el progreso
+            }
+        }
+    }
+    struct SoundModel {
+        let soundName: String
+        var bombSoundEffect: AVAudioPlayer?
+
+        init(soundName: String) {
+            self.soundName = soundName
+
+            if let path = Bundle.main.path(forResource: soundName, ofType: "m4a") {
+                let url = URL(fileURLWithPath: path)
+
+                do {
+                    bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+                    bombSoundEffect?.prepareToPlay() // Prepara el sonido para la reproducción
+                } catch {
+                    print("Error al cargar el sonido: \(error)")
+                }
+            } else {
+                print("Archivo de sonido no encontrado para \(soundName).")
+            }
+        }
+
+        func playSound() {
+            bombSoundEffect?.play() // Reproduce el sonido
+        }
+    }
+
+    func startTimer() {
+        self.gameStarted = true
+        self.timeRemaining = self.changeInterval // Reinicia el tiempo restante
+        self.startTimerRectangle()
+        
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                if self.timeRemaining > 0 {
+                    self.timeRemaining -= 1
+                } else {
+                    withAnimation {
+                        self.showImage = false // Oculta la imagen actual
+                        // Espera un pequeño tiempo para la transición de ocultar antes de mostrar la nueva imagen
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            if let cartaAleatoria = self.randomElement(from: &self.loteriaCartasEnJuego) {
+                                self.startTimerRectangle()
+                                self.cardName = cartaAleatoria.nombre
+                                self.cartasUsadas.append(cartaAleatoria) // Agrega la carta utilizada al array
+                                
+                                // Muestra la nueva imagen
+                                self.showImage = true
+                                
+                                // Inicializa el modelo de sonido y reproduce el sonido
+                                self.soundModel = SoundModel(soundName: self.cardName) // Cambia aquí el nombre del sonido
+                                self.soundModel?.playSound()
+                            }
+                            self.timeRemaining = self.changeInterval // Reinicia el tiempo restante
+                        }
+                    }
+                }
+            }
+    }
+
+
+    func randomElement(from array: inout [Carta]) -> Carta? {
+        guard !array.isEmpty else { return nil }
+        return array.remove(at: Int.random(in: 0..<array.count)) // Elimina y devuelve una carta aleatoria
+    }
+
+    func pauseGame() {
+        gamePaused = true
+          stopTimer()
+          stopTimerRectangle()
+      }
+      
+    func continueGame() {
+          startTimer()
+          gamePaused = false
+      }
+
+    // Otros métodos que manejen la lógica del juego...
+}
