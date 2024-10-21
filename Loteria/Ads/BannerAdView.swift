c81cf9867002acb2e4037b628f8cd3df79c16470
Tavo Avirez <gustavo.avila@gmail.com>
2024-10-20 18:37:28 -0600

    se crea una nueva vista, se agrega buscador de juegos, tmb se crea biblioteca

--- Loteria/Ads/BannerAdView.swift
+++ Loteria/Ads/BannerAdView.swift
@@ -13,8 +13,8 @@ struct BannerAd: UIViewControllerRepresentable {
         let viewController = UIViewController()
         let bannerView = GADBannerView(adSize: GADAdSizeBanner)
 
-//        bannerView.adUnitID = "ca-app-pub-1285793609420967/7086865752"
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716" // ID de prueba
+        bannerView.adUnitID = "ca-app-pub-1285793609420967/7086865752"
+//        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716" // ID de prueba
         bannerView.rootViewController = viewController
         viewController.view.addSubview(bannerView)
 
